a991ab13c6ea18f43b620284f6efc2ff9f053ecb
rlogrado <119961009+rlogrado@users.noreply.github.com>
2023-11-20 22:52:58 +0000

    Finalized OrderProduct Relation

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use App\Models\OrderProduct;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
@@ -14,11 +16,28 @@ class OrderController extends Controller
         return response()->json($orders);
     }
 
-    public function create(Request $request)
-    {
+
+public function create(Request $request)
+{
+    // Iniciando uma transação do banco de dados
+    DB::beginTransaction();
+
+    try {
+        // Validação dos dados do pedido (opcional, mas recomendado)
+        // $request->validate([...]);
+
+        // Criação do pedido
         $order = Order::create($request->only(['postal_code', 'total_price', 'order_date', 'shipped_date']));
 
+        // Processamento de cada produto
         foreach ($request->input('products', []) as $product) {
+            // Verifica se o produto existe
+            $productExists = Product::find($product['ProductID']);
+            if (!$productExists) {
+                throw new \Exception("Product with ID {$product['ProductID']} not found.", 404);
+            }
+
+            // Criação de OrderProduct
             OrderProduct::create([
                 'OrderID' => $order->id,
                 'ProductID' => $product['ProductID'],
@@ -26,8 +45,22 @@ class OrderController extends Controller
             ]);
         }
 
+        // Commit da transação
+        DB::commit();
+
         return response()->json($order->load('orderProducts.product'), 201);
+    } catch (\Exception $e) {
+        // Revertendo mudanças no banco de dados
+        DB::rollBack();
+
+        // Log do erro
+        //Log::error($e->getMessage());
+
+        // Resposta de erro
+        return response()->json(['error' => $e->getMessage()], $e->getCode() ?: 500);
     }
+}
+
 
     public function show($id)
     {
--- database/migrations/2023_11_17_215840_create_order_products_table.php
+++ database/migrations/2023_11_17_215840_create_order_products_table.php
@@ -9,10 +9,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('order_products', function (Blueprint $table) {
-            $table->id();
             $table->foreignId('OrderID')->constrained('orders');
             $table->foreignId('ProductID')->constrained('products');
-            $table->integer('quantity')->default(1);
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
